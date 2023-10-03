46fb24b30f190965160f2dbfd92baa5243e25339
Vishal Junghare <141499707+vishal-junghare@users.noreply.github.com>
2023-10-03 16:39:01 +0000

    Arry methods

---
+++ index.js
@@ -0,0 +1,11 @@
+let myArry=["vishal",29,5.6,true]
+//arry method sum diff methods 
+myArry[1] =6 // relpce the value 
+console.log(myArry)
+//find ths length 
+ let length=myArry.length;
+console.log(length) // find the myarry length of function use 
+myArry.pop(); // pop method
+console.log(myArry);
+myArry.push(4); //push methods
+console.log(myArry);
